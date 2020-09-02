bb9ad6c0a4d7b1af362018526408045aaf3addd6
unknown <pepeob12@gmail.com>
2020-09-02 13:46:24 -0300

    Desafio-Database-Upload

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,41 @@
+/* eslint-disable prettier/prettier */
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
+  @Column()
+  type: 'income' | 'outcome'
 
-  value: number;
+  @Column('decimal')
+  value: number
 
-  category_id: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
 
+  @Column()
+  category_id: string
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
